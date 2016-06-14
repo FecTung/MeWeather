8b70bcaeec376c9ba65ea0a375071a3202cfdc99
FecTung <1344243405@qq.com>
2016-06-14 16:25:45 +0800

    Update Connectivity.java

--- app/src/main/java/com/fec/me/meweather/util/Connectivity.java
+++ app/src/main/java/com/fec/me/meweather/util/Connectivity.java
@@ -4,69 +4,33 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.telephony.TelephonyManager;
 
-/**
- * Check device's network connectivity and speed
- * @author emil http://stackoverflow.com/users/220710/emil
- *
- */
 public class Connectivity {
 
-	/**
-	 * Get the network info
-	 * @param context
-	 * @return
-	 */
 	public static NetworkInfo getNetworkInfo(Context context){
 		ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
 		return cm.getActiveNetworkInfo();
 	}
 
-	/**
-	 * Check if there is any connectivity
-	 * @param context
-	 * @return
-	 */
 	public static boolean isConnected(Context context){
 		NetworkInfo info = Connectivity.getNetworkInfo(context);
 		return (info != null && info.isConnected());
 	}
 
-	/**
-	 * Check if there is any connectivity to a Wifi network
-	 * @param context
-	 * @return
-	 */
 	public static boolean isConnectedWifi(Context context){
 		NetworkInfo info = Connectivity.getNetworkInfo(context);
 		return (info != null && info.isConnected() && info.getType() == ConnectivityManager.TYPE_WIFI);
 	}
 
-	/**
-	 * Check if there is any connectivity to a mobile network
-	 * @param context
-	 * @return
-	 */
 	public static boolean isConnectedMobile(Context context){
 		NetworkInfo info = Connectivity.getNetworkInfo(context);
 		return (info != null && info.isConnected() && info.getType() == ConnectivityManager.TYPE_MOBILE);
 	}
 
-	/**
-	 * Check if there is fast connectivity
-	 * @param context
-	 * @return
-	 */
 	public static boolean isConnectedFast(Context context){
 		NetworkInfo info = Connectivity.getNetworkInfo(context);
 		return (info != null && info.isConnected() && Connectivity.isConnectionFast(info.getType(),info.getSubtype()));
 	}
 
-	/**
-	 * Check if the connection is fast
-	 * @param type
-	 * @param subType
-	 * @return
-	 */
 	public static boolean isConnectionFast(int type, int subType){
 		if(type==ConnectivityManager.TYPE_WIFI){
 			return true;
@@ -92,10 +56,6 @@ public class Connectivity {
 					return true; // ~ 1-23 Mbps
 				case TelephonyManager.NETWORK_TYPE_UMTS:
 					return true; // ~ 400-7000 kbps
-            /*
-             * Above API level 7, make sure to set android:targetSdkVersion
-             * to appropriate level to use these
-             */
 				case TelephonyManager.NETWORK_TYPE_EHRPD: // API level 11
 					return true; // ~ 1-2 Mbps
 				case TelephonyManager.NETWORK_TYPE_EVDO_B: // API level 9
